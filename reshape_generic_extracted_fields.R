54d82fc672479b94290f210b3366ebb6e45f0566
Daniel Spaude <daniel@spaude.de>
2018-12-09 16:28:23 +0100

    WIP

--- reshape_generic_extracted_fields.R
+++ reshape_generic_extracted_fields.R
@@ -21,13 +21,7 @@ reshape_by_resource_type = function(generic_fields, resource_type_str) {
 
 
 
-
-  
 generic_fields = read.csv("generic_field_extractions.csv")
-
-colnames(generic_fields)
-
-
 
 
 # Define predicate for relation fields for resource 'projects'
@@ -38,7 +32,7 @@ colnames(generic_fields)
 #   arrange(field_name)
 # as.character(distinct_field_names$field_name)
 
-# Besides the 'ProjectRelation: .*' pattern: in general, I identified the following fields which needs 
+# Besides the 'ProjectRelation: .*' pattern: in general, we identified the following fields which needs 
 # consideration for special handling (because they have relation character): 
 # Applicant
 # Applicants
@@ -77,15 +71,42 @@ colnames(generic_fields)
 # Subject Area
 
 
-# Or other approach: filter out all rows where the field_value starts with "<a
-# foo3 = generic_fields %>%
-  # filter(grepl("^<a.*$", field_value))
 
-
-
-# Desired table structure of project_relations: 
-# project_id | reference_resource_type | reference_resource_id | 
-#   relation_found_on (PROJECT or REFERENCE_RESOURCE) | relation_type ("Research Units", "As Spokesperson", ...)
+project_relation_field_names = c(
+  "Applicant",
+  "Applicants",
+  "Applying institution",
+  "Co-Applicant",
+  "Co-Applicants",
+  "Co-applicant",
+  "Co-applicant institution",
+  "Co-applicants",
+  "Cooperation partner",
+  "Cooperation partners",
+  "Deputy spokespeople",
+  "Deputy spokesperson",
+  "Foreign institution",
+  "Foreign spokespeople",
+  "Foreign spokesperson",
+  "Former applicant",
+  "Former applicants",
+  "Head",
+  "Heads",
+  "International Co-Applicant",
+  "International Co-Applicants",
+  "Participating Institution",
+  "Participating Person",
+  "Participating Persons",
+  "Participating institution",
+  "Participating scientist",
+  "Participating scientists",
+  "Participating university",
+  "Partner organisation",
+  "Project leader",
+  "Project leaders",
+  "Spokesperson",
+  "Spokespersons"
+)
 
 project_relations_from_non_project_pages = generic_fields %>% 
   filter(resource_type != "project") %>%
@@ -98,52 +119,11 @@ project_relations_from_non_project_pages = generic_fields %>%
   select(project_id, reference_resource_type, reference_resource_id, relation_type, relation_found_on)
 
 
-
-# Approach for the relations found on the project detail pages: 
-# 1. 
-# 2. 
-project_relation_field_names = c(
-  "Applicant", 
-  "Applicants", 
-  "Applying institution", 
-  "Co-Applicant", 
-  "Co-Applicants", 
-  "Co-applicant", 
-  "Co-applicant institution", 
-  "Co-applicants", 
-  "Cooperation partner", 
-  "Cooperation partners", 
-  "Deputy spokespeople", 
-  "Deputy spokesperson", 
-  "Foreign institution", 
-  "Foreign spokespeople", 
-  "Foreign spokesperson", 
-  "Former applicant", 
-  "Former applicants", 
-  "Head", 
-  "Heads", 
-  "International Co-Applicant", 
-  "International Co-Applicants", 
-  "Participating Institution", 
-  "Participating Person", 
-  "Participating Persons", 
-  "Participating institution", 
-  "Participating scientist", 
-  "Participating scientists", 
-  "Participating university", 
-  "Partner organisation", 
-  "Project leader", 
-  "Project leaders", 
-  "Spokesperson", 
-  "Spokespersons"
-  )
-# project_id | reference_resource_type | reference_resource_id | 
-#   relation_found_on (PROJECT or REFERENCE_RESOURCE) | relation_type ("Research Units", "As Spokesperson", ...)
 project_relations_from_project_pages = generic_fields %>% 
   filter(resource_type == "project") %>%
   filter(field_name %in% project_relation_field_names) %>%
   mutate(project_id = as.character(resource_id)) %>%
-  # reference_resource_type und reference_resource_id aus field_value extracten
+  # extract field_value from reference_resource_type and reference_resource_id
   mutate(reference_resource_type = str_match(field_value, '<a class="intern" href="/gepris/([a-z]*)/')[,2]) %>%
   mutate(reference_resource_id = str_match(field_value, '<a class="intern" href="/gepris/[a-z]*/(\\d*)')[,2]) %>%
   mutate(relation_type = field_name) %>%
@@ -151,22 +131,11 @@ project_relations_from_project_pages = generic_fields %>%
   select(project_id, reference_resource_type, reference_resource_id, relation_type, relation_found_on)
 
 
-str(project_relations_from_project_pages)
 # merge project_relations_from_project_pages and project_relations_from_non_project_pages
 project_relations = project_relations_from_project_pages %>% 
   bind_rows(project_relations_from_non_project_pages) %>% 
-
-
-# TODO: replace synonyms in project_relations_from_project_pages$relation_type (cluster synonyms (like c("Applicant", "Applicants")))
-
-# Reduce(f = "-", x = 1:6, accumulate = T)
-
-# synonym_groups = c(
-#   c("Applicant", "Applicants", "As Applicant")
-# )
-
-
-# I identified the following groups which should be 'normalised' to only one of their representations
+# replace synonyms in project_relations_from_project_pages$relation_type (cluster synonyms (like c("Applicant", "Applicants")))
+# I identified the following groups which should be 'normalised' to only one of their representations: 
 
 # Applicant
 # Applicants
@@ -248,34 +217,6 @@ cleaned_project_relations = project_relations %>%
   
 
 
-# foo = Reduce(function(project_relations, synonym_group){
-#   return(
-#     project_relations %>%
-#       mutate(relation_type = ifelse(relation_type %in% synonym_group, synonym_group[1], relation_type))
-#   )
-# }, project_relations_from_project_pages, accumulate = F)
-
-
-
-
-
-
-
-
-
-
-# str_match(project_relations$relation_type, 'As\\s(.*)')
-bar = project_relations %>%
-  select(relation_type) %>%
-  # mutate(relation_type = str_match(relation_type, '^As (.*)$')[,2])
-  distinct(relation_type) %>%
-  arrange(relation_type)
-
-
-# foo = project_relations_from_project_pages %>%
-#   filter(!grepl("^.*<a class.*$", field_value)) 
-  
-
 # TODO
 # Subject Areas and Participating subject areas should be handled separately and should be extracted into 
 # their own respective tables (also since there is no link-href string extraction needed for a referenced resource id)
@@ -284,22 +225,17 @@ project_subject_areas_field_names = c(
   "Subject Area"
 )
 
-projects = reshape_by_resource_type(generic_fields, "project")
+projects = reshape_by_resource_type(generic_fields %>%
+# For projects, we wan't to remove alle fields (that would be transformed now into columns) that 
+# have relationship character, as these are handled separately in the cleaned_project_relations table
+  filter(! field_name %in% project_relation_field_names) %>%
+  # Also the fields "Subject Area" and "Participating subject areas" will be handled separately in a relation table. 
+  # And we can ignore the field "Project identifier" (since it's core information is already covered by the column resource_id)
+  filter(! field_name %in% c("Subject Area", "Participating subject areas", "Project identifier"))
+                                      , "project")
 
 people = reshape_by_resource_type(generic_fields, "person")
-colnames(people)
 
 institutions = reshape_by_resource_type(generic_fields, "institution")
-colnames(institutions)
-
-
-# 
-  
-foo = generic_fields %>%
-  spread(field_name, field_value)
-
-generic_fields[1047013,]
-generic_fields[1047014,]
-generic_fields[1047015,]
 
 
